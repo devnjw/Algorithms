9b1ab882682520d5bdb94b0686ceac27bd3c6a98
21600212 <21600212@handong.edu>
2021-04-24 23:44:16 +0900

    Make Library repository for frequently used codes like split

---
+++ Library/Char2Int.cpp
@@ -0,0 +1,10 @@
+
+
+char ch1 = '1';
+int num1 = ch1 - '0';
+cout << num1;  //1
+
+
+char ch1 = '1';
+int numAscii = (int)ch1;
+cout << numAscii;  //49 ('1'의 아스키 코드 값)
---
+++ Library/split.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <sstream>
+#include <string>
+
+vector<string> split(string input, char delimiter) {
+    vector<string> answer;
+    stringstream ss(input);
+    string temp;
+
+    while (getline(ss, temp, delimiter)) {
+        answer.push_back(temp);
+    }
+
+    return answer;
+}
