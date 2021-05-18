16dcd3562776cafb06708206483875ee5446ac11
21600212 <21600212@handong.edu>
2021-05-18 16:08:07 +0900

    edit KMP

--- Library/KMP.cpp
+++ Library/KMP.cpp
@@ -3,7 +3,7 @@ int KMP(string str, string key){
     for(int i=0; i<str.size(); ++i){
         if(str[i]==key[0]){
             for(int j=1; j<key.size(); ++j){
-                if(str[++i]!=key[j]) break;
+                if(++i == str.size() || str[i]!=key[j]) break;
                 else if(j==key.size()-1) return i - j;
             }
         }
