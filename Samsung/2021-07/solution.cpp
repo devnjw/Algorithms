f6baa9e5ac3f65fbb52e61c4a3751c5ff52b72bc
21600212 <21600212@handong.edu>
2021-07-15 23:21:55 +0900

    samsung number baseball game

---
+++ Samsung/2021-07/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+#define N 4
+
+typedef struct {
+           int hit;
+           int miss;
+} Result;
+
+// API
+extern Result query(int guess[]);
+
+void doUserImplementation(int guess[]) {
+           // Implement a user's implementation function
+           //
+           // The array of guess[] is a return array that
+           // is your guess for what digits[] would be.
+    // guess[0] = 1;
+    // guess[1] = 1;
+    // guess[2] = 1;
+    // guess[3] = 1;
+    //
+    // Result result = query(guess);
+    //cout << result.hit;
+}
