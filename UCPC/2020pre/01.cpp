af52a0eff01fea59cfd55406d7363159e3004501
21600212 <21600212@handong.edu>
2021-07-29 16:04:39 +0900

    UCPC

--- UCPC/2020pre/01.cpp
+++ UCPC/2020pre/01.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main(){
   float a, b, c, d, e, f;
-  cin >> a >> b >> c >> d >> e >> f;
   float x, y;
+  
+  cin >> a >> b >> c >> d >> e >> f;
+
   x = (e*c - b*f)/(e*a - d*b);
   y = (a*f - d*c)/(a*e - d*b);
 
--- UCPC/2020pre/04.cpp
+++ UCPC/2020pre/04.cpp
@@ -1,1 +1,9 @@
-0
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+  return 0;
+}
