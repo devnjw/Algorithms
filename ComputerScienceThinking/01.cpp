488cfd40deb0eb7f7f0231b01ecb48a0c5f22008
21600212 <21600212@handong.edu>
2021-09-02 11:43:16 +0900

    컴과사 과제 01 최적화

--- ComputerScienceThinking/01.cpp
+++ ComputerScienceThinking/01.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(NULL);
+  clock_t start, end;
+  start = clock();
+
+  // ios::sync_with_stdio(false);
+  // cin.tie(NULL);
 
   int number;
   cin >> number;
 
   int cmp = 1;
-  int cnt = 1;
+  int cnt = 0;
   while(cmp < number){
     int tmp = 1;
     while(cmp/tmp){
-      if((cmp/tmp)%10==4)
+      if((cmp/tmp)%10==4){
         cmp += tmp;
+        cnt += tmp;
+      }
       tmp*=10;
     }
-    if(cmp==number) break;
-    cnt++;
     cmp++;
   }
 
-  cout << cnt;
+  cout << cmp - cnt;
+
+  end = clock();
+
+  printf("Timer : %.2f",((float)(end - start) / CLOCKS_PER_SEC));
 
   return 0;
 }
