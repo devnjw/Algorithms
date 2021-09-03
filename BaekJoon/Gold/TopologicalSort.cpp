30a35ff9a66fe3beff5b974cbc04ca491da9a0d0
21600212 <21600212@handong.edu>
2021-09-03 23:13:21 +0900

    Topological Sort

---
+++ BaekJoon/Gold/TopologicalSort.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <stack>
+
+#define SIZE 32000
+
+using namespace std;
+
+struct Node{
+  int data;
+  Node *next;
+};
+
+struct Node * arr[SIZE+1];
+
+void connect(int curr, int next){
+  Node * new_node = (Node*)malloc(sizeof(Node));
+  new_node->data = next;
+	new_node->next = NULL;
+
+  // Check first element
+  Node * tmp_node = arr[curr];
+  while(tmp_node->next!=NULL){
+    tmp_node = tmp_node->next;
+  }
+  tmp_node->next = new_node;
+}
+
+int N, M;
+int connected[SIZE+1];
+
+void print_linked_list_arr(){
+  Node * tmp_node;
+  for(int i=1; i<=N; ++i){
+    tmp_node = arr[i];
+    while(tmp_node != NULL){
+      cout << tmp_node->data << " - ";
+      tmp_node = tmp_node->next;
+    }
+    cout << endl;
+  }
+
+  cout << tmp_node->data << endl;
+}
+
+stack<int> st;
+
+void DFS(int n){
+  if(connected[n])
+      return;
+
+  st.push(n);
+
+  Node * tmp_node = arr[n];
+  while(tmp_node != NULL){
+    connected[tmp_node->data]--;
+    DFS(tmp_node->data);
+    tmp_node = tmp_node->next;
+  }
+}
+
+int main(){
+  cin >> N >> M;
+
+  for(int i=1; i<=SIZE; ++i){
+    Node * new_node = (Node*)malloc(sizeof(Node));
+    new_node->data = i;
+    new_node->next = NULL;
+    arr[i] = new_node;
+  }
+
+  int a, b;
+  for(int i=0; i<M; ++i){
+    cin >> a >> b;
+    connect(b, a);
+    connected[a]++;
+  }
+
+  for(int i=1; i<=N; ++i){
+    if(connected[i]==0){
+      DFS(i);
+    }
+  }
+
+  while(!st.empty()){
+    cout << st.top() << " ";
+    st.pop();
+  }
+
+  return 0;
+}
