28f1c8f1b40f68ef4ca03c2ae89a64a21a52b58b
21600212 <21600212@handong.edu>
2021-09-17 17:26:10 +0900

    컴과사 C2 optimized

--- ComputerScienceThinking/C2.cpp
+++ ComputerScienceThinking/C2.cpp
@@ -6,29 +6,27 @@ using namespace std;
 int N;
 int mountain[MAXSIZE][MAXSIZE];
 int difficulty[MAXSIZE][MAXSIZE];
+int directions[4][2] = {{0,-1},{0,1},{-1,0},{1,0}};
 
 int DFS(int x, int y){
-  if(difficulty[y][x]!=-1)
-    return difficulty[y][x];
-
   int diff = 0;
+  int tmp, tx, ty;
 
-  int tmp;
-  if(y-1 >= 0 && mountain[y][x] > mountain[y-1][x]){
-    tmp = DFS(x, y-1) + mountain[y][x] - mountain[y-1][x];
-    if(diff < tmp) diff = tmp;
-  }
-  if(y+1 < N && mountain[y][x] > mountain[y+1][x]){
-    tmp = DFS(x, y+1) + mountain[y][x] - mountain[y+1][x];
-    if(diff < tmp) diff = tmp;
-  }
-  if(x-1 >= 0 && mountain[y][x] > mountain[y][x-1]){
-    tmp = DFS(x-1, y) + mountain[y][x] - mountain[y][x-1];
-    if(diff < tmp) diff = tmp;
-  }
-  if(x+1 < N && mountain[y][x] > mountain[y][x+1]){
-    tmp = DFS(x+1, y) + mountain[y][x] - mountain[y][x+1];
-    if(diff < tmp) diff = tmp;
+  for(int i=0; i<4; ++i){
+    tx = x + directions[i][0];
+    ty = y + directions[i][1];
+
+    if(ty < 0 || ty >= N) continue;
+    if(tx < 0 || tx >= N) continue;
+
+    if(mountain[y][x] > mountain[ty][tx]){
+      if(difficulty[ty][tx]!=-1)
+        tmp = difficulty[ty][tx] + mountain[y][x] - mountain[ty][tx];
+      else
+        tmp = DFS(tx, ty) + mountain[y][x] - mountain[ty][tx];
+
+      if(diff < tmp) diff = tmp;
+    }
   }
 
   difficulty[y][x] = diff;
