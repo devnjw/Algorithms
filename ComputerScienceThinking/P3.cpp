9add8abc31556f99dc83339dc2ca7c0fa1e19621
21600212 <21600212@handong.edu>
2021-09-20 23:49:19 +0900

    컴과사 P3

---
+++ ComputerScienceThinking/P3.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#define MAX_SIZE 100000000
+
+using namespace std;
+
+int heap[MAX_SIZE];
+int heapSize = 0;
+
+int heapPush(int value)
+{
+	heap[heapSize] = value;
+
+	int current = heapSize;
+	while (current > 0 && heap[current] < heap[(current - 1) / 2])
+	{
+		int temp = heap[(current - 1) / 2];
+		heap[(current - 1) / 2] = heap[current];
+		heap[current] = temp;
+		current = (current - 1) / 2;
+	}
+
+	heapSize = heapSize + 1;
+
+	return 1;
+}
+
+int M;
+
+int main(){
+
+  int n;
+
+  do {
+
+      cin >> n;
+      heapPush(n);
+
+  } while (getc(stdin) == '\n');
+
+
+  // cin >> M;
+  // int tmp;
+  // while(cin >> tmp){
+  //   heapPush(tmp);
+  // }
+  //
+  
+  for(int i=0; i<heapSize; ++i){
+    cout << heap[i] << " ";
+  }
+
+  return 0;
+}
