197279db550f4aaaa5a127b0e23017e5a7cc7379
21600212 <21600212@handong.edu>
2021-11-11 22:03:09 +0900

    C8

---
+++ ComputerScienceThinking/C8.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+#define SIZE 10000
+
+using namespace std;
+
+pair<int, int> arr[SIZE];
+
+pair<int, int> get_range(int x, int y, int d){
+  pair<int, int> result;
+  result.first = x - sqrt(d*d - y*y);
+  result.second = x + sqrt(d*d - y*y);
+  return result;
+}
+
+int main(){
+  int N, d;
+  cin >> N >> d;
+
+  int x, y;
+  for(int i=0; i<N; ++i){
+    cin >> x >> y;
+    arr[i] = get_range(x, y, d);
+  }
+
+  int min_out = 20000;
+  int answer = 0;
+  int last_idx = 0;
+  for(int i=0; i<N; ++i){
+    if(arr[i].second < min_out)
+      min_out = arr[i].second;
+    if(min_out <= arr[i].first){
+      answer++;
+      last_idx = i;
+      min_out = 20000;
+    }
+  }
+
+  if(last_idx < N-1)
+    answer++;
+
+  cout << answer;
+
+  return 0;
+}
