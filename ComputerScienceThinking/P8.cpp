4d032c5621f1336d83d136da4391472976ccb30d
21600212 <21600212@handong.edu>
2021-11-16 16:37:00 +0900

    P8 clean code

--- ComputerScienceThinking/P8.cpp
+++ ComputerScienceThinking/P8.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define TYPE long long
+#define ll long long
 
 using namespace std;
 
-vector<pair<TYPE, TYPE> > people;
+vector<pair<ll, ll> > people;
+int N, T;
 
-bool cmp(pair<TYPE, TYPE> a, pair<TYPE, TYPE> b){
-  if(a.first > b.first) return true;
-  else if(a.first == b.first){
-    if(a.second < b.second){
-      return true;
-    }
-  }
-  return false;
+bool cmp(pair<ll, ll> a, pair<ll, ll> b){
+  if(a.first > b.first)
+    return true;
+  else if(a.first == b.first && a.second < b.second)
+    return true;
+  else
+    return false;
 }
 
-int N, T;
 int main(){
   cin >> N >> T;
-  TYPE p, v;
+  ll p, v;
   for(int i=0; i<N; ++i){
     cin >> p >> v;
     people.push_back(make_pair(p, v));
